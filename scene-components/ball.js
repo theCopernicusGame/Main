755e6233b32ef7d8287a057d971a495b9e97aa31
ScottMillerWesCoas <scott.t.miller1@gmail.com>
2016-08-19 20:19:12 -0700

    added collision functionality to the ball for use in point system

--- scene-components/ball.js
+++ scene-components/ball.js
@@ -1,7 +1,18 @@
 'use strict';
 
 var textureLoader = new THREE.TextureLoader();
-var ballGeometry = new THREE.SphereGeometry(.3, 28.8, 14.4);
+var ballGeometry = new THREE.SphereGeometry(.3, 28.8, 14.4), 
+handleCollision = function( collided_with, linearVelocity, angularVelocity ) {
+        switch ( ++this.collisions ) {
+          case 1:
+          console.log('collided_with', collided_with.position); 
+          this.material.color.setHex(0xcc8855);
+            break;
+            default: 
+            console.log('capturing addtnl collisions', collided_with.position, 'collisions', this.collisions); 
+  
+        }
+      }
 // var ballGeometry = new THREE.CubeGeometry( 1, 1, 1, 2, 2, 2 );
 // var ballGeometry = new THREE.IcosahedronGeometry( 40, 1 );
 var moonNormal  = textureLoader.load('assets/finalMoonPics/normal.jpg');
@@ -17,5 +28,10 @@ ball.castShadow = true;
 ball.position.z = -2;
 ball.position.x = 2;
 ball.position.y = 4;
+ball.collisions = 0; 
 ball.__dirtyPosition = true;
 ball.receiveShadow = true;
+ball.addEventListener( 'collision', handleCollision );
+
+
+
