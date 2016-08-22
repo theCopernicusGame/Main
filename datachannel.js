681fb5d857ab41e1879a90033d1bd467a30c88c8
ScottMillerWesCoas <scott.t.miller1@gmail.com>
2016-08-22 14:08:25 -0700

    changed port in dataChannel

--- datachannel.js
+++ datachannel.js
@@ -23,7 +23,7 @@ var moved = false;
 var dataChannel;
 
 // set up socket connection between client and server for signaling
-io = io.connect();
+io = io.connect(app);
 
 displaySignalMessage('Waiting for other player...')
 
