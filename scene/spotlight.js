eef1a38dea247281bab3fc21b18bda9a0cf2bb81
davidvorona <davidavorona@gmail.com>
2016-08-23 11:15:21 -0700

    demo code

---
+++ scene/spotlight.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var parameters = {
+  a: true,
+  x: -90, y: 20, z: 4,
+  bX: 0, bY: 20, bZ: -50,
+  rotateX: .000, rotateY: .0013, rotateZ: .000,
+  cRotateX: .000, cRotateY: .001,
+  xLight: 85, yLight: 60, zLight: 60,
+  intensity: 0
+};
+
+var spotLight =  new THREE.SpotLight( 0xF0F0F0 );
+spotLight.intesity = parameters.intensity;
+spotLight.castShadow = true;
+spotLight.position.set(230, 75, 15);
+
+var spotLight2 =  new THREE.SpotLight( 0xBDBDBD );
+spotLight2.position.set(8, 2, 1);
+spotLight2.castShadow = true;
+spotLight2.distance = 40;
+spotLight2.fov = 15;
+spotLight2.target.position.set( -.22, 2, -3.8 );
