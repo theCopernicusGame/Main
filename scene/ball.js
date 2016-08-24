36cb71390f405cffd5374b32245d15b50121314f
davidvorona <davidavorona@gmail.com>
2016-08-23 17:00:21 -0700

    cleaned up code, prepped to work on div display

--- scene/ball.js
+++ scene/ball.js
@@ -1,74 +1,23 @@
 'use strict';
 
+// work on collision logic
+
 var textureLoader = new THREE.TextureLoader();
-var ballGeometry = new THREE.SphereGeometry(.3, 28.8, 14.4),
-handleCollision = function( collided_with, linearVelocity, angularVelocity ) {
-        console.log(this.collisions + 1);
-        switch ( ++this.collisions ) {
-          case 3:
-            // console.log('HIT GROUND!', collided_with.position, 'ball pos', this.position, 'target pos', target.position);
-            console.log(user.pointFlag);
-            if ( user.pointFlag === true && ((this.position.x - target.position.x) > -2) && ((this.position.x - target.position.x) < 2)  && ((this.position.z - target.position.z) < 2)  && ((this.position.z - target.position.z) < 2) ){
-              user.points += 2;
-              user.pointFlag = false;
-              user.trackFlag = false;
-              if (user.player === "user_1") $('#p1Points').text(user.points);
-              else $('#p2Points').text(user.points);
-              dataChannel.send(JSON.stringify({ 'points': 2 }));
-              setTimeout(function() {
-                moved = false;
-                dataChannel.send(JSON.stringify({ 'moved': moved }));
-                dataChannel.send(JSON.stringify({ 'turn': user.myTurn }));
-                user.myTurn = false;
-                scene.remove(ball);
-                user.pointFlag = true;
-                this.collisions = 0;
-                addBall();
-              }, 2000)
-            }
-            else if ( user.pointFlag === true && ((this.position.x - target.position.x) > -3.5) && ((this.position.x - target.position.x) < 3.5)  && ((this.position.z - target.position.z) < 3.5)  && ((this.position.z - target.position.z) < 3.5) ){
-              user.points += 1;
-              user.pointFlag = false;
-              user.trackFlag = false;
-              if (user.player === "user_1") $('#p1Points').text(user.points);
-              else $('#p2Points').text(user.points);
-              dataChannel.send(JSON.stringify({ 'points': 1 }));
-              setTimeout(function() {
-                moved = false;
-                dataChannel.send(JSON.stringify({ 'moved': moved }));
-                dataChannel.send(JSON.stringify({ 'turn': user.myTurn }));
-                user.myTurn = false;
-                scene.remove(ball);
-                user.pointFlag = true;
-                this.collisions = 0;
-                addBall();
-               }, 5000)
-            }
-            else {
-              user.points += 1;
-              user.pointFlag = false;
-              user.trackFlag = false;
-              if (user.player === "user_1") $('#p1Points').text(user.points);
-              else $('#p2Points').text(user.points);
-              dataChannel.send(JSON.stringify({ 'points': 1 }));
-              user.pointFlag = false;
-              user.trackFlag = false;
-              setTimeout(function() {
-                moved = false;
-                dataChannel.send(JSON.stringify({ 'moved': moved }));
-                dataChannel.send(JSON.stringify({ 'turn': user.myTurn }));
-                user.myTurn = false;
-                scene.remove(ball);
-                user.pointFlag = true;
-                this.collisions = 0;
-                addBall();
-               }, 5000)
-            }
-
-        }
-    };
-
-// MOVE COLLISION/GAME LOGIC FROM HERE TO CORRECT FILE
+var ballGeometry = new THREE.SphereGeometry(.3, 28.8, 14.4);
+var handleCollision = function( collided_with, linearVelocity, angularVelocity ) {
+  switch ( ++this.collisions ) {
+    case 3:
+      if ( user.pointFlag === true && ((this.position.x - target.position.x) > -2) && ((this.position.x - target.position.x) < 2)  && ((this.position.z - target.position.z) < 2)  && ((this.position.z - target.position.z) < 2) ){
+        endTurnAndUpdate(2);
+      }
+      else if ( user.pointFlag === true && ((this.position.x - target.position.x) > -3.5) && ((this.position.x - target.position.x) < 3.5)  && ((this.position.z - target.position.z) < 3.5)  && ((this.position.z - target.position.z) < 3.5) ){
+        endTurnAndUpdate(1);
+      }
+      else {
+        endTurnAndUpdate(1);
+      }
+    }
+  };
 
 var moonNormal  = textureLoader.load('assets/finalMoonPics/normal.jpg');
 var moonMap = textureLoader.load('assets/finalMoonPics/moonPic.jpg');
--- scene/scene.js
+++ scene/scene.js
@@ -1,8 +1,3 @@
-// START THREE.JS
-//FOR TESTING POINTS
-// 'use strict';
-
-$(function() {
 Physijs.scripts.worker = 'lib/physijs_worker.js'; //webworker used to minimize latency re phys.js
 Physijs.scripts.ammo = 'ammo.js';
 
@@ -52,14 +47,12 @@ addBall = function() {
     ball.position.z = 0;
     ball.position.x = 5;
     ball.position.y = .3;
-    console.log('ball added!')
     ball.collisions = 0;
     scene.add( ball );
   } else {
     ball2.position.z = 0;
     ball2.position.x = -7;
     ball2.position.y = .3;
-    console.log('ball2 added!')
     ball.collisions = 0;
     scene.add( ball2 );
   }
@@ -127,7 +120,8 @@ scene.add( spotLight2 );
 
 function render() {
 
-  scene.simulate(); // run physics
+  // run physics
+  scene.simulate();
 
   earth.rotation.x += parameters.rotateX;
   earth.rotation.y -= parameters.rotateY;
@@ -144,7 +138,7 @@ function render() {
     var xRot = ball.rotation.x
     var yRot = ball.rotation.y
     var zRot = ball.rotation.z;
-    displayMessage('Height: ' + parseFloat(ball.position.y - .3).toFixed(3), 'Distance: ' + parseFloat(5 - ball.position.x).toFixed(3));
+    displayPosition('Height: ' + parseFloat(ball.position.y - .3).toFixed(3), 'Distance: ' + parseFloat(5 - ball.position.x).toFixed(3));
     sendPosition(xPos, yPos, zPos, xRot, yRot, zRot);
   }
 
@@ -153,14 +147,14 @@ function render() {
     ball2.position.x = message.position[0];
     ball2.position.y = message.position[1];
     ball2.position.z = message.position[2];
-    ball2.rotation.x = -(message.rotation[0]);
-    ball2.rotation.y = -(message.rotation[1]);
+    // ball2.rotation.x = -(message.rotation[0]); one of these is buggy
+    // ball2.rotation.y = -(message.rotation[1]);
     ball2.rotation.z = -(message.rotation[2]);
   }
 
-  // start sending condition, sets projectile motion
+  // start sending condition, sets projectile motion, testing purposes only
   if (keyboard[32]){
-    ball.setLinearVelocity(new THREE.Vector3(-5, 7, 0));
+    ball.setLinearVelocity(new THREE.Vector3(-10.5, 10.5, 0));
     moved = true;
     dataChannel.send(JSON.stringify({ 'moved': moved }));
     sendPosition((-7 + (5 - ball.position.x)), ball.position.y, ball.position.z, ball.rotation.x, ball.rotation.y, ball.rotation.z);
@@ -173,49 +167,7 @@ function render() {
     stopAnimation();
   }
 
-  if (delayedTrackerTime.counter >= 10 && delayedTrackerTime.flag === true){
-    console.log('sp1', trackerTime.counter);
-    ball.setLinearVelocity(new THREE.Vector3(-3, 5, 0));
-    delayedTrackerTime.flag = false;
-    delayedTrackerTime.counter = 0;
-    moved = true;
-    dataChannel.send(JSON.stringify({ 'moved': moved }));
-    sendPosition((-7 + (5 - ball.position.x)), ball.position.y, ball.position.z, ball.rotation.x, ball.rotation.y, ball.rotation.z);
-    demo.clear();
-  }
-
-  if (delayedTrackerTime.counter >= 5 && delayedTrackerTime.counter < 10 && delayedTrackerTime.flag === true){
-    console.log('sp2', delayedTrackerTime.counter);
-    ball.setLinearVelocity(new THREE.Vector3(-8.4, 12, 0));
-    delayedTrackerTime.flag = false;
-    delayedTrackerTime.counter = 0;
-    moved = true;
-    dataChannel.send(JSON.stringify({ 'moved': moved }));
-    sendPosition((-7 + (5 - ball.position.x)), ball.position.y, ball.position.z, ball.rotation.x, ball.rotation.y, ball.rotation.z);
-    demo.clear();
-  }
-
-  if (delayedTrackerTime.counter > 3 && delayedTrackerTime.counter < 5 && delayedTrackerTime.flag === true){
-    console.log('sp3', trackerTime.counter);
-    ball.setLinearVelocity(new THREE.Vector3(-2, 18, 0));
-    delayedTrackerTime.flag = false;
-    delayedTrackerTime.counter = 0;
-    moved = true;
-    dataChannel.send(JSON.stringify({ 'moved': moved }));
-    sendPosition((-7 + (5 - ball.position.x)), ball.position.y, ball.position.z, ball.rotation.x, ball.rotation.y, ball.rotation.z);
-    demo.clear();
-   }
-
-  if (delayedTrackerTime.counter > 0 && delayedTrackerTime.counter <= 3 && delayedTrackerTime.flag === true){
-    console.log('sp4', trackerTime.counter);
-    ball.setLinearVelocity(new THREE.Vector3(-9, 13, 0));
-    delayedTrackerTime.flag = false;
-    delayedTrackerTime.counter = 0;
-    moved = true;
-    dataChannel.send(JSON.stringify({ 'moved': moved }));
-    sendPosition((-7 + (5 - ball.position.x)), ball.position.y, ball.position.z, ball.rotation.x, ball.rotation.y, ball.rotation.z);
-    demo.clear();
-   }
+  if (delayedTrackerMatches.flag === true && user.trackFlag === true) sendProjectile(delayedTrackerMatches.counter);
 
   var spaceScene = requestAnimationFrame( render );
   renderer.render( scene, camera );
@@ -240,20 +192,40 @@ $('#bg').prepend( renderer.domElement );
 
 var throwBall = $( "<button id='throwBall'>Click anywhere to throw!</button>" );
 if (user.player === "user_1") {
-  var pointsDiv = $( "<div id='pointsDiv'>Player 1 Points: <span id ='p1Points'>" + user.points + "</span><br>Player 2 Points: <span id='p2Points'>" + otherUser.points + " </span> </div>" );
+  var pointsDiv = $( "<div id='pointsDiv'>Player 1 Points: <span id ='p1Points'>" + user.points + "</span><br>Player 2 Points: <span id='p2Points'>" + user.otherPoints + " </span> </div>" );
 } else {
-  var pointsDiv = $( "<div id='pointsDiv'>Player 1 Points: <span id ='p1Points'>" + otherUser.points + "</span><br>Player 2 Points: <span id='p2Points'>" + user.points + " </span> </div>" );
+  var pointsDiv = $( "<div id='pointsDiv'>Player 1 Points: <span id ='p1Points'>" + user.otherPoints + "</span><br>Player 2 Points: <span id='p2Points'>" + user.points + " </span> </div>" );
 }
 $('#bg').append(pointsDiv);
 $('#bg').append(throwBall);
 $('#pointsDiv').fadeOut(0);
 $('#throwBall').fadeOut(0);
 
-});
-
 function sendPosition(x, y, z, xr, yr, zr) {
   var toSend = { 'type': 'ballPos', 'position': [ x, y, z ], 'rotation': [ xr, yr, zr ] };
   dataChannel.send(JSON.stringify(toSend));
+}
+
+/* current velocity tiers:
+2-12, 12-21, 21-70, 70-200
+*/
+function determineVelocity(trackerCount) {
+  if (trackerCount < 21) trackerCount = 21;
+  const trackerToVelocityMult = 525;
+  var velocity = (1/trackerCount) * trackerToVelocityMult;
+  return velocity;
+}
+
+function sendProjectile(trackerCount) {
+  var velocity = determineVelocity(trackerCount);
+  ball.setLinearVelocity(new THREE.Vector3(-velocity, velocity, 0));
+  delayedTrackerMatches.flag = false;
+  user.trackFlag = false;
+  delayedTrackerMatches.counter = 0;
+  moved = true;
+  dataChannel.send(JSON.stringify({ 'moved': moved }));
+  sendPosition((-7 + (5 - ball.position.x)), ball.position.y, ball.position.z, ball.rotation.x, ball.rotation.y, ball.rotation.z);
+  demo.clear();
 }
 
 function stopAnimation() {
