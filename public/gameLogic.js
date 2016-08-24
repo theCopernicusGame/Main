8c47e12c9d5c62628e2703c995eb4f63964b0675
ScottMillerWesCoas <scott.t.miller1@gmail.com>
2016-08-23 20:15:09 -0700

    updated tracking software to remove dupes, split data into tiers for velocity, added picture taking functionality

--- public/gameLogic.js
+++ public/gameLogic.js
@@ -4,7 +4,7 @@
 var user = {};
 
 function chooseUser() {
-  if (peerFound == true) {
+  if (peerFound === true) {
     return ["user_2", false];
   } else {
     return ["user_1", true];
@@ -13,15 +13,45 @@ function chooseUser() {
 
 user.player = chooseUser()[0];
 user.myTurn = chooseUser()[1];
-
-if (user.player === "user_2") displaySignalMessage("You've joined Player 1!")
-
-user.points = 0;
 user.pointFlag = true;
 user.trackFlag = false;
+user.points = 0;
+user.otherPoints = 0;
+
+if (user.player === "user_2") displaySignalMessage("You've joined Player 1!");
+
+function endTurnAndUpdate(points) {
+  user.points += points;
+  user.pointFlag = false;
+  if (user.player === "user_1") $('#p1Points').text(user.points);
+  else $('#p2Points').text(user.points);
+  dataChannel.send(JSON.stringify({ 'points': points }));
+  setTimeout(function() {
+    moved = false;
+    dataChannel.send(JSON.stringify({ 'moved': moved }));
+    dataChannel.send(JSON.stringify({ 'turn': user.myTurn }));
+    user.myTurn = false;
+    user.pointFlag = true;
+    scene.remove(ball);
+    addBall();
+  }, 2000)
+}
+
+function updateAndStartTurn() {
+  user.myTurn = received.turn;
+  scene.remove(ball2);
+  addBall();
+  user.pointFlag = true;
+}
+
+function updateOtherPoints() {
+  user.otherPoints += received.points;
+  if (user.player === "user_1") $('#p2Points').text(user.otherPoints);
+  else $('#p1Points').text(user.otherPoints);
+}
 
 function addScene() {
-  $('#gamescript').append( `<script type=` + `"text/javascript"` + ` src=` + `"scene/scene.js"` + `></script>` );
+  $('#gamescript').append( `<script type=` + `"text/javascript"` + ` src=` + `"./scene/scene.js"` + `></script>` );
 }
 
 setTimeout(addScene, 2000);
