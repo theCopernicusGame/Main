fd59aabe16987c8be999d9ab3e0ce03629281d86
davidvorona <davidavorona@gmail.com>
2016-08-29 17:07:48 -0700

    fixes

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,26 @@
 // requirements:
 // express.io integrates socket.io with express
 // body/cookieParser for client keys as unique links/rooms
+var fs = require('fs');
+// var options = {
+//   key: fs.readFileSync('./cert/copernicus.pem'),
+//   cert: fs.readFileSync('./cert/2_www.thecopernicusgame.com.crt'),
+//   ca: fs.readFileSync('./cert/1_Intermediate.crt'),
+//   passphrase: 'copernicus'
+// };
+
 var express = require('express.io');
 var app = express();
 app.http().io();
+
 var bodyParser = require('body-parser');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 
-app.use(express.static(path.join(__dirname + '/../')));
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname + '/../')));
 
 // currently test page url, to be home page url
 // get req.params for specific room url
--- galaxyScene/jupiter.js
+++ galaxyScene/jupiter.js
@@ -1,26 +1,26 @@
- 
+
     jupiterOrbitRadius = 530,
     jupiterOrbitAngle = -20,
     jupiterOrbitSpeed = 0.05;
 
     jupiterAndGanymede = new THREE.Object3D();
-    // jupiterAndGanymede.rotation.z = 23.439281 * Math.PI / 180; 
-    jupiterAndGanymede.receiveShadow = true; 
+    // jupiterAndGanymede.rotation.z = 23.439281 * Math.PI / 180;
+    jupiterAndGanymede.receiveShadow = true;
 
     ganymedeOrbitRadius = 32,
     ganymedeOrbitAngle = 20,
     ganymedeOrbitSpeed = 0.7;
- 
-    var jupiterGeometry = new THREE.SphereGeometry(23, 28.8, 15); 
-    var jupiterMap  = textureLoader.load('assets/planetPics/jupiterMap.jpg');
+
+    var jupiterGeometry = new THREE.SphereGeometry(23, 28.8, 15);
+    var jupiterMap  = textureLoader.load('assets/planetPics/jupitermap.jpg');
     var jupiterTexture =  new THREE.MeshPhongMaterial( { map: jupiterMap} );
-    Jupiter = new THREE.Mesh( jupiterGeometry, jupiterTexture); 
-    Jupiter.receiveShadow = false; 
+    Jupiter = new THREE.Mesh( jupiterGeometry, jupiterTexture);
+    Jupiter.receiveShadow = false;
 
 
     Ganymede = new THREE.Mesh(new THREE.SphereGeometry(3, 16, 15),  new THREE.MeshPhongMaterial({
 
-    map: textureLoader.load('assets/planetPics/plutomap1k.jpg'), 
+    map: textureLoader.load('assets/planetPics/plutomap1k.jpg'),
     bumpMap: textureLoader.load('assets/planetPics/plutobump1k.jpg')
-  
+
 }));
