79fffd6f15073917e8822ffffda54ecd94b78965
ScottMillerWesCoas <scott.t.miller1@gmail.com>
2016-09-07 11:21:35 -0700

    moved data to top left corner, added fadeIn, fadeOut to angle input

--- scene/scene.js
+++ scene/scene.js
@@ -1,6 +1,21 @@
 Physijs.scripts.worker = '/lib/physijs_worker.js'; //webworker used to minimize latency re phys.js
 Physijs.scripts.ammo = 'ammo.js';
 
+var brightCounter = 0; 
+//Shimmer functionality for angle input 
+var baloon = $('.fadeBright');
+     function runIt() {
+      brightCounter++; 
+      if (brightCounter < 3){
+       baloon.animate({opacity:'1'}, 1500);
+       baloon.animate({opacity:'0.2'}, 1500, runIt);
+       }
+       else baloon.animate({opacity:'1'}, 1500);
+    }
+    runIt();
+
+
+
 function onWindowResize() {
    camera.aspect = window.innerWidth / window.innerHeight;
    camera.updateProjectionMatrix();
