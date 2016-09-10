bbc87719a2a411cf9539d60e7787d58e9782ac00
davidvorona <davidavorona@gmail.com>
2016-09-10 15:54:37 -0700

    audio and demo stuff

--- public/dataChannel.js
+++ public/dataChannel.js
@@ -59,18 +59,20 @@ var peerFound = false;
 // set up socket connection between client and server for signaling
 io = io.connect();
 
-//COLLECTING AUDIO FOR CHAT
-navigator.mediaDevices.getUserMedia({audio: true, video: false}).then(function(stream) {
-  localStream = stream;
-  var audioTracks = localStream.getAudioTracks();
-  // if MediaStream has reference to microphone
-  if (audioTracks[0]) {
-    audioTracks[0].enabled = true;
-  }
-  // emits event to server setting up unique room
-  // DIRECTIONS, to server.js
-  if (singleplayer === false) io.emit('ready', {"signal_room": SIGNAL_ROOM });
-});
+//COLLECTING AUDIO FOR CHAT AND START SIGNALING
+if (singleplayer === false) {
+  navigator.mediaDevices.getUserMedia({audio: true, video: false}).then(function(stream) {
+    localStream = stream;
+    var audioTracks = localStream.getAudioTracks();
+    // if MediaStream has reference to microphone
+    if (audioTracks[0]) {
+      audioTracks[0].enabled = true;
+    }
+    // emits event to server setting up unique room
+    // DIRECTIONS, to server.js
+    io.emit('ready', {"signal_room": SIGNAL_ROOM });
+  });
+}
 
 if (singleplayer === false) {
   displaySignalMessage('Waiting for other player...')
