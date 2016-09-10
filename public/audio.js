0add9dc09da2f527518a369fc522d9c65121caf2
ScottMillerWesCoas <scott.t.miller1@gmail.com>
2016-09-10 15:45:41 -0700

    updated audio functionality re mulitple SendLocalDescription error

---
+++ public/audio.js
@@ -0,0 +1,19 @@
+var callButton = document.querySelector('button#callButton');
+var hangupButton = document.querySelector('button#hangupButton');
+hangupButton.disabled = true;
+var audioTracks;
+var audio = document.querySelector('#audio');
+callButton.onclick = unmute;
+hangupButton.onclick = hangUp;
+
+//Start AUDIO
+function unmute(){
+  console.log('UNMUTING AUDIO',audioTracks[0]);
+  audioTracks[0].enabled = true;
+  hangupButton.disabled = false;
+}
+//STOP AUDIO
+function hangUp() {
+  console.log('MUTING AUDIO',audioTracks[0]);
+  audioTracks[0].enabled = false;
+};
--- scene/loaderAndLoadingScreen.js
+++ scene/loaderAndLoadingScreen.js
@@ -56,9 +56,12 @@ function allItemsLoaded() {
   $('.onepix-imgloader').fadeOut();
   $('.loading-container > *:not(.onepix-imgloader)').fadeTo(35000, 100);
   setTimeout(function() {
-    $('.loading-container').css('background-image', 'inherit');
+    $('.loading-container').css('background-image', 'url(/assets/marssky.jpg)');
     $('.loading-container').css('background-repeat', 'no-repeat');
     $('.loading-container').css('background-size', 'cover');
+    $('body').css('background-image', 'url(/assets/marssky.jpg)');
+    $('body').css('background-repeat', 'no-repeat');
+    $('body').css('background-size', 'cover');
   }, 2000);
   $('#start-scan').css('visibility', 'visible');
 }
