b27a934dc7ae2481b7b2f11f0839d45f21fceace
ScottMillerWesCoas <scott.t.miller1@gmail.com>
2016-09-13 18:14:35 -0700

    added 8 tests for ball in game scene

--- scene/sceneObjects.js
+++ scene/sceneObjects.js
@@ -34,12 +34,29 @@ function ballBuilder(){
   return ball; 
 }
 
-function test(){
-  return {'hello': 2}; 
+
+function earthBuilder(){
+  var textureLoader = new THREE.TextureLoader();
+  var earthGeometry = new THREE.SphereGeometry(36, 28.8, 14.4);
+  var earthMap  = textureLoader.load('/assets/planetPics/earthmap1k.jpg');
+  var earthBump = textureLoader.load('/assets/planetPics/earthbump1k.jpg');
+  var earthSpec = textureLoader.load('/assets/planetPics/earthspec1k.jpg');
+  var earthTexture = new THREE.MeshPhongMaterial( { map: earthMap, bumpMap: earthBump, specularMap: earthSpec} );
+  var earth = new THREE.Mesh(earthGeometry, earthTexture );
+
+  earth.position.z = 80;
+  earth.position.x = -300;
+  earth.position.y = -10;
+  earth.castShadow = true;
+  return earth; 
 }
+
+
+
+
 
 
 if (typeof exports !== 'undefined')
 {
-  module.exports = {test, ballBuilder}; 
+  module.exports = {ballBuilder, earthBuilder}; 
 }
--- testing/gameSceneTests.js
+++ testing/gameSceneTests.js
@@ -6,83 +6,90 @@ var peerFound, resArr = [];
  
 
   
-  describe('ball object', function(){
-    it('should exist', function(done){
-        var output = sceneObjects.ballBuilder(); 
-        expect(output).to.exist; 
-        done(); 
-    });
-    it('should contain geometry property', function(done){
-        var output = sceneObjects.ballBuilder(); 
-        expect(output).to.have.any.keys('geometry'); 
-        done(); 
-    });
-    it('should contain position property', function(done){
-        var output = sceneObjects.ballBuilder(); 
-        resArr = []; 
-        for (var x in output){
-          if (x === 'position') resArr.push(output[x]); 
+describe('ball object', function(){
+  it('should exist', function(done){
+      var output = sceneObjects.ballBuilder(); 
+      expect(output).to.exist; 
+      done(); 
+  });
+  it('should contain geometry property', function(done){
+      var output = sceneObjects.ballBuilder(); 
+      expect(output).to.have.any.keys('geometry'); 
+      done(); 
+  });
+  it('should contain position property', function(done){
+      var output = sceneObjects.ballBuilder(); 
+      resArr = []; 
+      for (var x in output){
+        if (x === 'position') resArr.push(output[x]); 
+      }
+      expect(output).to.have.any.keys('position'); 
+      done(); 
+  });
+   it('position property should be set to {x: 0, y: 0, z: 0}', function(done){
+      var output = sceneObjects.ballBuilder(); 
+      var testObj = {}; 
+      for (var x in output){
+        if (x === 'position'){
+          for (var inner in output[x]){
+              if (inner === 'x') testObj[inner] = output[x][inner];
+              else if (inner === 'y') testObj[inner] = output[x][inner];
+              else if (inner === 'z') testObj[inner] = output[x][inner];
+          }   
         }
-        expect(output).to.have.any.keys('position'); 
-        done(); 
-    });
-     it('position property should be set to {x: 0, y: 0, z: 0}', function(done){
-        var output = sceneObjects.ballBuilder(); 
-        var testObj = {}; 
-        for (var x in output){
-          if (x === 'position'){
-            for (var inner in output[x]){
-                if (inner === 'x') testObj[inner] = output[x][inner];
-                else if (inner === 'y') testObj[inner] = output[x][inner];
-                else if (inner === 'z') testObj[inner] = output[x][inner];
-            }   
-          }
+      }
+      expect([testObj]).to.deep.include.members([{'x': 0, 'y': 0, 'z': 0}]); 
+      done(); 
+  });
+  it('should contain materials property', function(done){
+      var output = sceneObjects.ballBuilder();
+      expect(output).to.have.any.keys('material'); 
+      done(); 
+  }); 
+  it('material should be MeshPhong', function(done){
+      var output = sceneObjects.ballBuilder(); 
+      var testObj = {}; 
+      for (var x in output){
+        if (x === 'material'){
+          for (var inner in output[x]){
+              if (inner === 'type') testObj[inner] = output[x][inner];
+          }   
         }
-        expect([testObj]).to.deep.include.members([{'x': 0, 'y': 0, 'z': 0}]); 
-        done(); 
-    });
-    it('should contain materials property', function(done){
-        var output = sceneObjects.ballBuilder();
-        expect(output).to.have.any.keys('material'); 
-        done(); 
-    }); 
-    it('material should be MeshPhong', function(done){
-        var output = sceneObjects.ballBuilder(); 
-        var testObj = {}; 
-        for (var x in output){
-          if (x === 'material'){
-            for (var inner in output[x]){
-                if (inner === 'type') testObj[inner] = output[x][inner];
-            }   
-          }
-        }
-        expect([testObj]).to.deep.include.members([{'type':'MeshPhongMaterial'}]); 
-        done(); 
-    }); 
-       it('should have an initial rotation of {x: 0, y: 0, z: 0}', function(done){
-         var output = sceneObjects.ballBuilder(); 
-        var testObj = {}; 
-        for (var x in output){
-          if (x === 'rotation'){
-            for (var inner in output[x]){
-                if (inner === 'x') testObj[inner] = output[x][inner];
-                else if (inner === 'y') testObj[inner] = output[x][inner];
-                else if (inner === 'z') testObj[inner] = output[x][inner];
-            }   
-          }
-        }
-        expect([testObj]).to.deep.include.members([{'x': 0, 'y': 0, 'z': 0}]); 
-        done(); 
-    });  
-       it('should have a collisions property set to 0', function(done){
-         var output = sceneObjects.ballBuilder(); 
-        var testObj = {}; 
-        for (var x in output){
-          if (x === 'collisions') testObj[x] = output[x];  
-        }
-        expect([testObj]).to.deep.include.members([{'collisions': 0}]); 
-        done(); 
-    });  
+      }
+      expect([testObj]).to.deep.include.members([{'type':'MeshPhongMaterial'}]); 
+      done(); 
+  }); 
+ it('should have an initial rotation of {x: 0, y: 0, z: 0}', function(done){
+   var output = sceneObjects.ballBuilder(); 
+  var testObj = {}; 
+  for (var x in output){
+    if (x === 'rotation'){
+      for (var inner in output[x]){
+          if (inner === 'x') testObj[inner] = output[x][inner];
+          else if (inner === 'y') testObj[inner] = output[x][inner];
+          else if (inner === 'z') testObj[inner] = output[x][inner];
+      }   
+    }
+  }
+  expect([testObj]).to.deep.include.members([{'x': 0, 'y': 0, 'z': 0}]); 
+  done(); 
+  });  
+  it('should have a collisions property set to 0', function(done){
+  var output = sceneObjects.ballBuilder(); 
+  var testObj = {}; 
+  for (var x in output){
+    if (x === 'collisions') testObj[x] = output[x];  
+  }
+  expect([testObj]).to.deep.include.members([{'collisions': 0}]); 
+  done(); 
+  });  
 }); 
+describe('earth object', function(){
+  it('should exist', function(done){
+      var output = sceneObjects.earthBuilder(); 
+      expect(output).to.exist; 
+      done(); 
+  });
+    }); 
 
 
