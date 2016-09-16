533e4a329014d30dc4aba5cd96f48a29916e7dc4
ScottMillerWesCoas <scott.t.miller1@gmail.com>
2016-09-16 16:12:12 -0700

    added clarifying code to Sun, Venus and Uranus files

--- galaxyScene/sun.js
+++ galaxyScene/sun.js
@@ -1,3 +1,7 @@
+//Each planet has it's own orbitRadius (dist. from sun), Orbit Angle (initial starting position in it's elliptical pattern) and speed
+//Each planet is using a combination of maps - color map is the visual look, bump map adds the appearance of mountains (topography)
+//specular map adds reflectiveness re light. 
+
 var sunGeometry = new THREE.SphereGeometry(55, 28.8, 20); 
 var sunMap  = textureLoader.load('assets/planetPics/sunmap.jpg');
 var sunTexture =  new THREE.MeshPhongMaterial( { map: sunMap} );
