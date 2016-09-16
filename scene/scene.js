9962aa13567d66320062c8f2b25cc89d1f48ab20
davidvorona <davidavorona@gmail.com>
2016-09-16 16:45:18 -0700

    scene and gameLogic comments

--- scene/scene.js
+++ scene/scene.js
@@ -1,6 +1,9 @@
-Physijs.scripts.worker = '/lib/physijs_worker.js'; //webworker used to minimize latency re phys.js
+// initializes Phys.js library web worker for physics calculations
+// Phys.js is built on top of ammo.js
+Physijs.scripts.worker = '/lib/physijs_worker.js';
 Physijs.scripts.ammo = 'ammo.js';
 
+// to handle mid-game window resizing
 function onWindowResize() {
    camera.aspect = window.innerWidth / window.innerHeight;
    camera.updateProjectionMatrix();
@@ -8,13 +11,26 @@ function onWindowResize() {
    render();
 }
 
-// in case window size changes
 window.addEventListener( 'resize', onWindowResize, false );
 
-//to collect possible user change in angle;
-var userVelocity, spaceScene;
-var camera, renderer, mesh;
+// to handle user keyboard input
 var keyboard = {};
+
+function keyDown(event){
+  keyboard[event.keyCode] = true;
+}
+
+function keyUp(event){
+  keyboard[event.keyCode] = false;
+}
+
+window.addEventListener('keydown', keyDown);
+window.addEventListener('keyup', keyUp);
+
+// initializes variables for scene
+// keyboard for spacebar functionality
+var spaceScene;
+var camera, renderer, mesh;
 scene = new Physijs.Scene;
 scene.setGravity(new THREE.Vector3( 0, user.changeGravityValue, 0 ));
 scene.addEventListener(
@@ -24,6 +40,7 @@ scene.addEventListener(
   }
 );
 
+// set parameters for entire scene
 renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true } );
 renderer.setClearColor(0x000000, 0);
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -42,10 +59,8 @@ camera.lookAt(new THREE.Vector3(0,3,0))
 // add earth w/ clouds to scene
 // scene.add( earth );
 
-// add demo lighting
-if (isDemo === true && user.player === "user_1") scene.add(ambientLight);
 
-// add ball
+// add ball, depending on user type
 addBall = function() {
   if (user.myTurn === true) {
     ball.position.z = 0;
@@ -63,6 +78,7 @@ addBall = function() {
 }
 addBall();
 
+
 //add target
 scene.add(target);
 scene.add(cap1);
@@ -71,6 +87,7 @@ scene.add(cap3);
 scene.add(cap4);
 scene.add(cap5);
 scene.add(cap6);
+
 
 // add astronaut
 objLoader.load( '/assets/astronaut/Astronaut.obj', function ( object ) {
@@ -88,7 +105,9 @@ objLoader.load( '/assets/astronaut/Astronaut.obj', function ( object ) {
   scene.add(object);
 });
 
-// add moon floor
+
+// add planet landscape
+// TODO: make dynamic, so user can choose planet
 var floorImage = new THREE.Texture();
 var floorMap = new THREE.Texture();
 
@@ -96,7 +115,6 @@ imgLoader.load('/assets/finalMoonPics/marsColor.bmp', function(img) {
   floorImage.image = img;
   floorImage.needsUpdate = true;
 });
-
 
 imgLoader.load('/assets/finalMoonPics/marsNormal.jpg', function(img) {
   floorMap.image = img;
@@ -115,58 +133,39 @@ objLoader.load( '/assets/finalMoonPics/marsFloor.obj', function ( object ) {
   scene.add( object );
 });
 
-//fake floor (invisible)
+
+// add fake floor (invisible), OBJs have no physics attributes
 scene.add( fakeFloor );
+
 
 // add lighting
 scene.add( spotlight );
 scene.add( spotlight2 );
-//scene.add( ambientLight );
+// add demo lighting
+if (isDemo === true && user.player === "user_1") scene.add(ambientLight);
+
 
 function render() {
 
-  // run physics
+  // run physics and set gravity
   scene.simulate();
   scene.setGravity(new THREE.Vector3( 0, user.changeGravityValue, 0 ));
 
+  // if earth exists, make that baby spin!
   earth.rotation.x += parameters.rotateX;
   earth.rotation.y -= parameters.rotateY;
   earth.rotation.z += parameters.rotateZ;
-
+  // add cool clouds to earth
   cloudMesh.rotation.x -= parameters.cRotateX;
   cloudMesh.rotation.y -= parameters.cRotateY;
 
-  // continue send condition
-  if (moved === true && user.myTurn === true) {
-    var xPos = -7 + (5 - ball.position.x);
-    var yPos = ball.position.y;
-    var zPos = ball.position.z;
-    var xRot = ball.rotation.x
-    var yRot = ball.rotation.y
-    var zRot = ball.rotation.z;
-    displayPosition('Height: ' + parseFloat(ball.position.y - .3).toFixed(3) + 'm', 'Distance: ' + parseFloat(5 - ball.position.x).toFixed(3) + 'm');
-    if (turnEnded === false) {
-      storePosition();
-      checkBadThrow();
-    }
-    if (singleplayer === false) {
-      sendPosition(xPos, yPos, zPos, xRot, yRot, zRot);
-    }
-  }
 
-  // received condition
-  if (moved === true && user.myTurn === false) {
-    ball2.position.x = message.position[0];
-    ball2.position.y = message.position[1];
-    ball2.position.z = message.position[2];
-    ball2.rotation.z = -(message.rotation[2]);
-  }
-
-  // start sending condition, sets projectile motion, testing purposes only
+  // start sending condition for spacebar, sets initial impulse for projectile motion
   if (user.spaceBarFlag === true) {
     throwProjectile();
   }
 
+  // start sending condition for tracking, sets initial impulse for projectile motion
   if (delayedTrackerMatches.trackFlag === true && user.trackFlag === true) {
     if (isDemo === true) userVelocity = 4.5;
     else {
@@ -192,22 +191,37 @@ function render() {
     throwProjectile();
   }
 
+  // if you've already given ball initial impulse, simply continue sending
+  if (moved === true && user.myTurn === true) {
+    var xPos = -7 + (5 - ball.position.x);
+    var yPos = ball.position.y;
+    var zPos = ball.position.z;
+    var xRot = ball.rotation.x
+    var yRot = ball.rotation.y
+    var zRot = ball.rotation.z;
+    displayPosition('Height: ' + parseFloat(ball.position.y - .3).toFixed(3) + 'm', 'Distance: ' + parseFloat(5 - ball.position.x).toFixed(3) + 'm');
+    if (turnEnded === false) {
+      storePosition();
+      checkBadThrow();
+    }
+    if (singleplayer === false) {
+      sendPosition(xPos, yPos, zPos, xRot, yRot, zRot);
+    }
+  }
+
+  // received condition, set your ball's position to mirror of other's
+  if (moved === true && user.myTurn === false) {
+    ball2.position.x = message.position[0];
+    ball2.position.y = message.position[1];
+    ball2.position.z = message.position[2];
+    ball2.rotation.z = -(message.rotation[2]);
+  }
+
   spaceScene = requestAnimationFrame( render );
   renderer.render( scene, camera );
 }
 
 render();
-
-function keyDown(event){
-  keyboard[event.keyCode] = true;
-}
-
-function keyUp(event){
-  keyboard[event.keyCode] = false;
-}
-
-window.addEventListener('keydown', keyDown);
-window.addEventListener('keyup', keyUp);
 
 
 // for appending game messages to the DOM
@@ -227,7 +241,7 @@ function determineVelocity(velocity, angle) {
 }
 
 function throwProjectile() {
-  var userAngle = $('#inputAngle').val(); 
+  var userAngle = $('#inputAngle').val();
   var velocity = determineVelocity(userVelocity, userAngle);
   t = performance.now();
   ball.setLinearVelocity(new THREE.Vector3(velocity[0], velocity[1], 0));
@@ -245,5 +259,3 @@ function throwProjectile() {
   }
   demo.clear();
 }
-
-
